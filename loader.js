5eca01730575ff21fe056e667df5c20ce115214f
alvingerolao <156308629+alvingerolao@users.noreply.github.com>
2025-07-11 20:25:44 -0700

    Delete loader.js

--- loader.js
+++
@@ -1,5 +0,0 @@
- // Simulate a delay (e.g., fetching data)
- setTimeout(function() {
-    document.getElementById("loader").style.display = "none";
-    document.getElementById("content").style.display = "block";
-}, 3000); // Simulates a 3-second loading time
